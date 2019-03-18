c79c96f24bade601b71171c6bedf5a946468022b
ajal <ajalvaruthunny@gmail.com>
2019-03-18 16:13:04 +0100

    working write service

--- src/client_trial_me.py
+++ src/client_trial_me.py
@@ -6,8 +6,10 @@ import rospy
 
 
 from teleop_rover.srv import *
+from teleop_rover.msg import *
 
-
+node = Address()
+data= TypeValue()
 
 def disconnect_client():
 	
@@ -32,13 +34,44 @@ def connect_client(endpoint):
 	except rospy.ServiceException, e:
 		print "Service call failed: %s"%e
 
+def write_client(node,data):
+	print "inside write_client"
+	rospy.wait_for_service('/opcua/opcua_client/write')
+
+	try:			
+		write = rospy.ServiceProxy('/opcua/opcua_client/write',Write)
+		resp1 = write(node,data)
+		return resp1.success, resp1.error_message
+	except rospy.ServiceException, e:
+		print "Service call failed: %s"%e
+
+
 if __name__ == "__main__":
 	print "hola"
-#to connect
+	node.nodeId = 'ns=2;i=8'
+	node.qualifiedName = 'MyVariable'
+
+	data.type = 'float64'
+	data.bool_d = False
+	data.int8_d = 0
+	data.uint8_d = 0
+	data.int16_d = 0
+	data.uint16_d = 0
+	data.int32_d = 0
+	data.uint32_d = 0
+	data.int64_d =0
+	data.uint64_d = 0
+	data.float_d = 0.0
+	data.double_d =4.1
+	data.string_d =''
+
+	write_client(node,data)
+'''#to connect
 	endpoint = "opc.tcp://localhost:4840"	
 	connect_client(endpoint)
 #to disconnect	
-	disconnect_client()	
-	print "end" 
+	disconnect_client()'''
+ 
+
 
 
